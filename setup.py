af21189c1a5c986b8d9cb97e03ea71f3f223937d
Kelvin Sundli <kelvin.sundli@cognite.com>
2020-03-23 22:51:30 +0100

    add @Vr93 to authors

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='xlogger',
-      version='0.1',
+      version='0.2',
       description='Cross-project logger instance.',
       url='https://github.com/ks93/xlogger',
-      author='Kelvin Sundli',
-      author_email='kelvin.sundli@gmail.com',
+      author='Kelvin Sundli <kelvin.sundli@gmail.com>, Vegard Rogne, <vegardrogne@hotmail.com>',
+      author_email='kelvin.sundli@gmail.com, vegardrogne@hotmail.com',
       license='MIT',
       packages=['xlogger'],
       zip_safe=False)
